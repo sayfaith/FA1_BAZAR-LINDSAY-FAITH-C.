ef36abd7ea8a18ec7c24db13ac71e7b73ce36b17
sayfaith <124319969+sayfaith@users.noreply.github.com>
2023-02-03 10:20:37 +0800

    Add files via upload

---
+++ bazar.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+km = input("Enter kilometer: ")
+KM = float(km)
+klm = 1.61
+miles = KM/klm
+print("Miles: %.2f"%miles)
+
+
+# In[ ]:
+
+
+sec = input("Enter seconds: ")
+min = input("Enter minutes: ")
+km = input("Enter kilometers: ")
+
+SEC = int(sec)
+MIN = int(min)
+KM = float(km)
+
+klm = 1.61
+miles= KM/klm
+
+min
+sec
+
+
